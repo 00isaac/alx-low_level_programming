6131186dad558bce5ba16c800e1007870adfc9b8
Edem Isaac <edemisaac144@gmail.com>
2022-12-12 16:10:30 -0800

    another commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Description: Return value is one
- * 
+ *
  * Return: 0
  */
 int main(void)
 {
-		char data[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        char data[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-			fprintf(stderr, "%s", data);
-				return (1);
+	fprintf(stderr, "%s", data);
+	return (1);
 }
